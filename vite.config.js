b71624dcc2dc1ecd75beb7b3005b9b01c13cabd4
Soham Katdare <sohamkatdare8@gmail.com>
2024-09-15 07:27:00 -0400

    fix production routing

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,16 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         org_home: resolve(__dirname, 'src/org_home/index.html'),
         org_signup: resolve(__dirname, 'src/org_signup/index.html'),
+        ind_signup: resolve(__dirname, 'src/ind_signup/index.html'),
+        ind_login: resolve(__dirname, 'src/ind_login/index.html'),
+        org_login: resolve(__dirname, 'src/org_login/index.html'),
+        org_events: resolve(__dirname, 'src/org_events/index.html'),
+        org_announcements: resolve(__dirname, 'src/org_announcements/index.html'),
+        org_analytics: resolve(__dirname, 'src/org_analytics/index.html'),
+        ind_home: resolve(__dirname, 'src/ind_home/index.html'),
+        ind_findEvents: resolve(__dirname, 'src/ind_findEvents/index.html'),
+        ind_events: resolve(__dirname, 'src/ind_events/index.html'),
+        compose_event: resolve(__dirname, 'src/compose_event/index.html'),
       },
       output: {
         assetFileNames: (assetInfo) => {
